9baf171b32e168ccae03579a5c13a5caeb4651d4
noaether <67566122+noaether@users.noreply.github.com>
2025-01-21 15:33:18 -0500

    init

---
+++ src/main/java/ca/noae/leashed/Leashed.java
@@ -0,0 +1,17 @@
+package ca.noae.leashed;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Leashed extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
